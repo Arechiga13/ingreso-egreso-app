9b1253e94619ecff3b9544bbc98572cb73e79251
Manuel Antonio Arechiga Lopez <arechigalopezmanuel@hotmail.com>
2019-06-03 16:03:19 -0500

    Seccion 7 lista

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCuSdofHlKTCHhUkLlJiZDKigP09rIywuw',
+    authDomain: 'ingreso-egreso-app-fc518.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-fc518.firebaseio.com',
+    projectId: 'ingreso-egreso-app-fc518',
+    storageBucket: 'ingreso-egreso-app-fc518.appspot.com',
+    messagingSenderId: '144293279476',
+    appId: '1:144293279476:web:e3cb464cdec7fdfb'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCuSdofHlKTCHhUkLlJiZDKigP09rIywuw',
+    authDomain: 'ingreso-egreso-app-fc518.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-fc518.firebaseio.com',
+    projectId: 'ingreso-egreso-app-fc518',
+    storageBucket: 'ingreso-egreso-app-fc518.appspot.com',
+    messagingSenderId: '144293279476',
+    appId: '1:144293279476:web:e3cb464cdec7fdfb'
+  }
 };
 
 /*
