09f9681e89743be68055e39ee40de053b0e9133c
Manuel Antonio Arechiga Lopez <arechigalopezmanuel@hotmail.com>
2019-06-04 13:19:34 -0500

    Seccion 8 lista - inicio redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
+// NGRX
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducer';
+
 // Environments
 import { environment } from '../environments/environment';
 
@@ -44,6 +49,11 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,26 +7,46 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
 
+import { Store } from '@ngrx/store';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.accions';
+
 import Swal from 'sweetalert2';
 import { User } from './user.model';
+import { AppState } from '../app.reducer';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private userSubscription: Subscription = new Subscription();
+
   constructor( private afAuth: AngularFireAuth,
                private router: Router,
-               private afDB: AngularFirestore
+               private afDB: AngularFirestore,
+               private store: Store<AppState>
                 ) { }
 
   initAuthListener() {
     this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
-      console.log(fbUser);
+      if ( fbUser ) {
+        this.userSubscription = this.afDB.doc(`${ fbUser.uid }/usuario`).valueChanges()
+          .subscribe( (usuarioObj: any) => {
+            const newUser = new User(usuarioObj);
+            this.store.dispatch( new SetUserAction(newUser));
+            console.log(newUser);
+          });
+      } else {
+        this.userSubscription.unsubscribe();
+      }
+
     });
   }
 
   crearUsuario( nombre: string, email: string, password: string ) {
+    this.store.dispatch( new ActivarLoadingAction() );
     this.afAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then( resp => {
@@ -41,23 +61,28 @@ export class AuthService {
           .set( user )
           .then( () => {
             this.router.navigate(['/']);
+            this.store.dispatch( new DesactivarLoadingAction() );
           });
       })
       .catch( error => {
         // console.error(error);
+        this.store.dispatch( new DesactivarLoadingAction() );
         Swal.fire('Error en el login', error.message, 'error');
       });
   }
 
   login( email: string, password: string ) {
+    this.store.dispatch( new ActivarLoadingAction() );
     this.afAuth.auth
       .signInWithEmailAndPassword(email, password)
       .then( resp => {
         // console.log(resp);
         this.router.navigate(['/']);
+        this.store.dispatch( new DesactivarLoadingAction() );
       })
       .catch( error => {
         // console.error(error);
+        this.store.dispatch( new DesactivarLoadingAction() );
         Swal.fire('Error en el login', error.message, 'error');
       });
   }
