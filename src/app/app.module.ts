4afe54becfd236db46bd2b6892a3adc4dfc5d0f8
Manuel Antonio Arechiga Lopez <arechigalopezmanuel@hotmail.com>
2019-06-10 13:44:58 -0500

    Seccion 10 lista -app prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 
 // Modulos
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
 
-// Graficas
-import { ChartsModule } from 'ng2-charts';
+// Modulos personalizados
+import { AuthModule } from './auth/auth.module';
+// import { IngresoEgresoModule } from './ingreso-egreso/ingreso-egreso.module';
 
 // NGRX
 import { StoreModule } from '@ngrx/store';
@@ -22,40 +21,18 @@ import { appReducers } from './app.reducer';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
-import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { OrdenIngresoEgresoPipe } from './ingreso-egreso/orden-ingreso-egreso.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    DetalleComponent,
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent,
-    OrdenIngresoEgresoPipe
   ],
   imports: [
     BrowserModule,
+    AuthModule,
+    // IngresoEgresoModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule,
-    ChartsModule,
     StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/app.reducer';
+// import { AppState } from '../../app.reducer';
 import { Subscription } from 'rxjs';
 import { IngresoEgreso } from '../ingreso-egreso.model';
-import { Label, MultiDataSet } from 'ng2-charts';
+import { Label } from 'ng2-charts';
+
+import * as fromIngresoEgreso from '../ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-estadistica',
@@ -23,12 +25,12 @@ export class EstadisticaComponent implements OnInit {
   public doughnutChartLabels: Label[] = ['Ingresos', 'Egresos'];
   public doughnutChartData: number[] = [];
 
-  constructor( private store: Store<AppState> ) { }
+  constructor( private store: Store<fromIngresoEgreso.AppState> ) { }
 
   ngOnInit() {
     this.subscription = this.store.select('ingresoEgreso')
       .subscribe( ingresoEgreso => {
-        this.contarIngresoEgreso( ingresoEgreso.items )
+        this.contarIngresoEgreso( ingresoEgreso.items );
       });
   }
 
